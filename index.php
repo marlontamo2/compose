8358be524dd7873d30542fc750cac1925a83e214
marlontamo2 <marlontamo2@gmail.com>
2024-06-29 11:47:35 +0800

    added env

--- index.php
+++ index.php
@@ -13,4 +13,16 @@ $userService->addUser(1, "John Doe");
 $userService->addUser(2, "Jane Smith");
 $userService->removeUser(1);
 $userService->listUsers();
+echo"<hr>";
  //test123
+//  $env =["GITHUB_CLIENT_ID"=> githubclient,
+//  "GITHUB_CLIENT_SECRET"=> githubsecret,
+$env = file(__DIR__.'/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach($env as $value)
+{
+  $value = explode('=', $value);
+  define($value[0], $value[1]);
+  
+}
+echo constant("GITHUB_CLIENT_ID")."<br>";
+echo constant("GITHUB_CLIENT_SECRET");
